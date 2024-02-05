2c563f87c7057ac3054248d2713989dc6d5301f6
Gokul <gokulmk0602@gmail.com>
2024-02-05 10:26:34 -0500

    Added a row in result

--- popup.js
+++ popup.js
@@ -7,11 +7,31 @@ function exportToCSV() {
   let accValue = document.getElementById("inputValue").value;
   let idValue = document.getElementById("inputValue2").value;
 
+  let currentDate = new Date();
+  let day = currentDate.getDate();
+  let month = currentDate.getMonth() + 1; // Months are zero-based, so add 1
+  let year = currentDate.getFullYear();
+
+  let formattedDate = `${day}/${month}/${year}`;
+  let formattedMonth = `${month} ${year}`;
+  let analyst = "Gokul";
+  let caseOrCall = "case";
+  let subj = "Subject;";
+
   if (accValue.trim() === "" || idValue.trim() === "") {
     alert("Please enter a value.");
     return;
   }
-  var arr = [accValue, idValue];
+
+  let arr = [
+    formattedDate,
+    formattedMonth,
+    analyst,
+    caseOrCall,
+    idValue,
+    accValue,
+    subj,
+  ];
   var csvContent = "data:text/csv;charset=utf-8," + encodeURIComponent(arr);
   var link = document.createElement("a");
   link.href = csvContent;
